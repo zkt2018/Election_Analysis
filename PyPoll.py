dfac9f16680dc7f25603242e26780069b1fa2cf1
zkt2018 <zky1384@yahoo.ca>
2021-07-18 16:55:34 -0400

    Adding file

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,4 @@
-import os
+# import os
 # # Create a filename variable to a direct or indirect path to the file.
 # file_to_save = os.path.join("analysis", "election_analysis.txt")
 # # Using the open() function with the "w" mode we will write data to the file.
@@ -32,26 +32,112 @@ import os
 #     txt_file.write("\nArapahoe\nDenver\nJefferson")
 
 # Add our dependencies.
+# import csv
+# import os
+# # Assign a variable to load a file from a path.
+# file_to_load = os.path.join("Resources", "election_results.csv")
+# # Assign a variable to save the file to a path.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+#     # Read the file object with the reader function.
+#     file_reader = csv.reader(election_data)
+#     # # To get the first item from each row
+#     # # for row in file_reader:
+#     # #     print(row[0])
+#     # Print the header row (will also be used to skip the header row when retrieving data)
+#     headers = next(file_reader)
+#     #print(headers)
+#     # Print each row in the CSV file.
+#     for row in file_reader:
+#         print(row)
+
+
+# Add our dependencies.
 import csv
 import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
+# 1. Initializing a variable to count up all the votes
+total_votes = 0
+# Candidate Options
+candidate_options = []
 # Open the election results and read the file.
+
+# Declare the empty dictionary.
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-    # Print each row in the CSV file.
-    # for row in file_reader:
-    #     print(row)
-    # # To get the first item from each row
-    # # for row in file_reader:
-    # #     print(row[0])
-    # Print the header row.
+
+    # Print the header row (will also be used to skip the header row when retrieving data)
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # # Add the candidate name to the candidate list (prints the name of candidates from all the rows)
+        # candidate_options.append(candidate_name)
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# 3. Print the total votes.
+print(total_votes)
+
+# # 3. Print the candidate list.
+# print(candidate_options)
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    # print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.") 
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    # Determine winning vote count and candidate
+    # 1. Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # 2. If true then set winning_count = votes and winning_percent = vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # 3. Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
 
 
 
