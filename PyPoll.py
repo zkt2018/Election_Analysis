0ac5f9f13b53864d268294c3a15404cc999452e9
zkt2018 <zky1384@yahoo.ca>
2021-07-20 23:07:41 -0400

    Adding final files

--- PyPoll.py
+++ PyPoll.py
@@ -4,7 +4,7 @@ import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis_pypoll.txt")
 # Initialize a total vote counter.
 total_votes = 0
 # Candidate options and candidate votes.
